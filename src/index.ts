39299fd0215babc4ab72c336d8db9df7c12b74d9
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-05 21:38:53 -0300

    feat: added express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Executando");
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+    PORT,
+    () => {
+        console.log(`APP RUNNING ON PORT ${PORT}`);
+    }
+);
+
+
